05253a0086d52d9a3a252ea58c4e684eb75239a9
slawless08 <samuel.l.lawless@gmail.com>
2021-10-11 20:48:02 -0400

    Added authentication to profile schema

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -20,6 +20,20 @@ const profileSchema = new Schema({
     groups: [Group.schema]
 });
 
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    };
+
+    next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password){
+    return bcrypt.compare(password, this.password);
+};
+
 const Profile = model('Profile', profileSchema);
 
 module.exports = Profile;
