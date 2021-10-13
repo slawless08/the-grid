9905c5d39cbc2f8784ec6c091c947647865ba8d3
slawless08 <samuel.l.lawless@gmail.com>
2021-10-13 17:15:53 -0400

    Fixed a couple small bugs and added group and post seeds.js files.

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,17 @@
 const db = require('../config/connection');
-const { Profile } = require('../models');
+const { Profile, Post, Group } = require('../models');
 const profileSeeds = require('./profileSeeds.json');
+const postSeeds = require('./postSeeds.json');
+const groupSeeds = require('./groupSeeds.json');
 
 db.once('open', async () => {
     try {
         await Profile.deleteMany({});
         await Profile.create(profileSeeds);
+        await Post.deleteMany({});
+        await Post.create(postSeeds);
+        await Group.deleteMany({});
+        await Group.create(groupSeeds);
 
         console.log('Profiles seeded');
         process.exit(0);
