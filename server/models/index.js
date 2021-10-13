a1fb72c88038119def7ff499dd00cb8d24cc50b7
slawless08 <samuel.l.lawless@gmail.com>
2021-10-13 15:47:46 -0400

    Added a few changes to the models.

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const Profile = require('./Profile');
+const Group = require('./Group');
+const Post = require('./Posts');
 
-module.exports = { Profile };
+module.exports = { Profile, Group, Post };
